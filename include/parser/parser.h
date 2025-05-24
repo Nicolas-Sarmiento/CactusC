4f9201153a0eadd232bb42bf7c5d397a02db535c
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2025-05-24 17:14:15 -0500

    (Add) Functions are related to CFG

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -1,1 +1,18 @@
 //Definir las reglas gramaticales
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "parser/astNode.h"
+#include "parser/tokenstream.h"
+#include "error_handler/error_handler.h"
+
+typedef struct parseresult{
+    ASTNode* node;
+    Result result;
+} ParseResult;
+
+
+ParseResult* parse_program(TokenStream* stream);
+
+
+#endif
---
+++ src/parser/parser.c
@@ -0,0 +1,73 @@
+#include "parser/parser.h"
+
+ParseResult* parse_program(TokenStream* stream){
+    //expect int ID () <block>
+}
+
+ParseResult* parse_block( TokenStream* stream){
+    // Expect { <stmts> }
+}
+
+ParseResult* parse_stmts( TokenStream* stream){
+    
+}
+
+ParseResult* parse_stmt( TokenStream* stream){
+
+}
+
+ParseResult* parse_stmts( TokenStream* stream){
+
+}
+
+ParseResult* parse_var_init( TokenStream* stream){
+
+}
+
+ParseResult* parse_var_decl( TokenStream* stream){
+
+}
+
+ParseResult* parse_if( TokenStream* stream){
+
+}
+
+ParseResult* parse_while( TokenStream* stream){
+
+}
+
+ParseResult* parse_return( TokenStream* stream){
+
+}
+
+ParseResult* parse_expr( TokenStream* stream){
+
+}
+
+ParseResult* parse_expr_or( TokenStream* stream){
+
+}
+
+ParseResult* parse_expr_and( TokenStream* stream){
+
+}
+
+ParseResult* parse_equality( TokenStream* stream){
+
+}
+
+ParseResult* parse_factor( TokenStream* stream){
+
+}
+
+ParseResult* parse_term( TokenStream* stream){
+
+}
+
+ParseResult* parse_unary( TokenStream* stream){
+
+}
+
+ParseResult* parse_primary( TokenStream* stream){
+
+}
